9808f008a5399169757ac7b222979d711e57fe14
Uri3lArchangel <solomonhemigadon@gmail.com>
2023-03-13 20:00:41 -0400

    Auth Page and forms with responsive query with apple and google corrections

--- components/Frontend/src/Auth/AppleButton.tsx
+++ components/Frontend/src/Auth/AppleButton.tsx
@@ -4,7 +4,7 @@ import appleIcon_lg from '../../Images/Auth/appleIcon_lg.png'
 
 function AButton() {
   return (
-    <button  className='bg-black justify-center align-middle text-white text-sm flex mx-auto px-4 py-2 rounded-full my-3 border border-pink-400/20 appleAuth_btn'>
+    <button  className='bg-black justify-center items-center text-white text-sm flex mx-auto px-4 h-16 rounded-full my-3 border border-pink-400/20 appleAuth_btn'>
    <div className='appleIcon w-5 mr-3'>
   <Image src={appleIcon_lg} layout='intrinsic' alt='google' />
   </div>
--- components/Frontend/src/Auth/GoogleButton.tsx
+++ components/Frontend/src/Auth/GoogleButton.tsx
@@ -5,7 +5,7 @@ import googleIcon_lg from '../../Images/Auth/googleIcon_lg.png'
 function GButton() {
   return (
     
-    <button className='bg-black flex justify-center align-middle text-white text-sm mx-auto px-4 py-2 rounded-full my-3 border border-pink-400/20 googleAuth_btn'>
+    <button className='bg-black flex justify-center items-center text-white text-sm mx-auto px-4 h-16 rounded-full my-3 border border-pink-400/20 googleAuth_btn'>
     <div className='googleIcon w-5 mr-3'>
   <Image src={googleIcon_lg} layout='intrinsic' alt='google' />
   </div>
